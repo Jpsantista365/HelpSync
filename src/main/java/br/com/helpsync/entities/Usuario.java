2f130e76f282ed0c5b8be5c345436b95601fe7fa
João Paulo Lopes Pinho <125623615+Jpsantista365@users.noreply.github.com>
2024-12-09 20:16:46 -0400

    Fisrt commit

---
+++ src/main/java/br/com/helpsync/entities/Usuario.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package br.com.helpsync.entities;
+
+/**
+ *
+ * @author 06248596140
+ */
+
+public class Usuario {
+
+    private String username;
+    private String password;
+    private String email;
+    private String telefone;
+}
